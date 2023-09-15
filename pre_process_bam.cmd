5e9332f99008995fb4ec9f7529d969784d9468d0
solcos <92781950+solcos@users.noreply.github.com>
2023-09-15 08:52:25 +0200

    Update pre_process_bam.cmd

--- pre_process_bam.cmd
+++ pre_process_bam.cmd
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 #SBATCH --job-name=pre_process
 #SBATCH --output=./logs/pre_process_logs.out
@@ -8,6 +8,13 @@
 hostname >&2
 printf START >&2; uptime >&2
 date >&2
+
+## Dates variable for log files name
+date_var=$(date +%Y%m%d%H%M%S)
+
+## Change log file name with date time
+mv ./logs/pre_process_logs.out ./logs/pre_process_logs_${date_var}.out
+mv ./logs/pre_process_error.out ./logs/pre_process_error_${date_var}.out
 
 ## Load modules
 module load samtools/latest java/12.0.2 gatk/4.1.8.1 
@@ -43,9 +50,22 @@ if [ -z ${yq_path}/yq ]; then
 fi
 
 ## REQUIRED SETTINGS IN CONF.YAML
-SAMPLES=$(${yq_path}/yq '.REQUIRED_SETTINGS.SAMPLES' $conf_file) ## Path to the reference genome of the input BAM file
+TMP_DIR=$(${yq_path}/yq '.REQUIRED_SETTINGS.TMP_DIR' $conf_file) # Set the path to the temprorary files directory.
+
+# Check if 'TMP_DIR' parameter was passed to the script
+if ! [ -d $TMP_DIR ]; then
+    echo -e "${red}ERROR${clear}    ${date_var} Parameter 'TMP_DIR' in 'conf.yaml' is required or set incorrectly."
+    exit_abnormal
+elif ! [ -w $TMP_DIR ]; then
+    echo -e "${red}ERROR${clear}    ${date_var} The 'TMP' folder does not have write permissions."
+    exit_abnormal
+else
+    # Set full dir
+    TMP_DIR=$(echo $(readlink -f "$TMP_DIR"))
+fi
 
 ## Check if the 'SAMPLES' file exists and if it has any problem
+SAMPLES=$(${yq_path}/yq '.REQUIRED_SETTINGS.SAMPLES' $conf_file) ## Path to the reference genome of the input BAM file
 ## Set full dir
 SAMPLES=$(echo $(readlink -f "${SAMPLES}"))
 if ! [ -e $SAMPLES ]; then
@@ -54,9 +74,9 @@ if ! [ -e $SAMPLES ]; then
 else
    
     ## Remove empty lines in samples.txt file
-    sed '/^$/d' $SAMPLES | grep -v "^#" > .edited_samples.txt
+    sed '/^$/d' $SAMPLES | grep -v "^#" > ${TMP_DIR}/.edited_samples.txt
 
-    if ! [ -s .edited_samples.txt ]; then ## Check if the variable is empty
+    if ! [ -s ${TMP_DIR}/.edited_samples.txt ]; then ## Check if the variable is empty
         echo -e "${red}ERROR${clear}    ${date_var} Parameter 'SAMPLES' in 'conf.yaml' has no samples in it or are malformed configure."
     else
 
@@ -66,7 +86,7 @@ else
             # Check if the 'line' in the 'samples.csv' file is a path
             if ! [ -f "$line" ]; then
                 echo -e "${red}ERROR${clear}    ${date_var} Input sample '${line}' does not have a proper path or it's invalid."
-                rm -f .edited_samples.txt
+                rm -f ${TMP_DIR}/.edited_samples.txt
                 exit 1
             fi        
  
@@ -85,20 +105,20 @@ else
                     if ! [ -z "${header}" ];
                     then
                         echo -e "${red}ERROR${clear}    ${date_var} Input file (${input_sample}) does not have a valid header and/or body."
-                        rm -f .edited_samples.txt
+                        rm -f ${TMP_DIR}/.edited_samples.txt
                         exit 1
                     fi
                 else
                     echo -e "${red}ERROR${clear}    ${date_var} Input file (${input_sample}) does not have the proper extension (.bam) or it can't be read properly."
-                    rm -f .edited_samples.txt
+                    rm -f ${TMP_DIR}/.edited_samples.txt
                     exit 1
                 fi
                 echo -e "${red}ERROR${clear}    ${date_var} Can't start the pipeline for ${input_sample}, input file or path are not correct."
-                rm -f .edited_samples.txt
+                rm -f ${TMP_DIR}/.edited_samples.txt
                 exit 1
             fi
 
-        done < .edited_samples.txt ## End while loop
+        done < ${TMP_DIR}/.edited_samples.txt ## End while loop
     fi
 fi
 
@@ -107,7 +127,7 @@ while IFS= read -r bam_file;
 do
  
   # Extract the file names from the absolute paths and count their occurrences
-  duplicate_files=$(awk -F/ '{print $NF}' .edited_samples.txt | sort | uniq -cd)
+  duplicate_files=$(awk -F/ '{print $NF}' ${TMP_DIR}/.edited_samples.txt | sort | uniq -cd)
 
   # Check if there are any duplicate file names
   if [[ -n $duplicate_files ]]; then
@@ -116,24 +136,25 @@ do
       duplicate_files=$(echo "$duplicate_files" | sed 's/^/\t/')
       echo -e "${red}ERROR${clear}   Duplicate file names found:"
       echo "$duplicate_files"
-      rm -f .edited_samples.txt
+      rm -f ${TMP_DIR}/.edited_samples.txt
       exit 1
   fi
 
   filename=$(basename ${bam_file} .bam)
-  cp .pre_process_bam_template.cmd .job_${filename}.cmd
-  sed -i "s+template+$bam_file+g" .job_${filename}.cmd ## Sed can use any separator rather than '/'
-  sed -i "s/filename/${filename}/g" .job_${filename}.cmd
-  sed -i "s/number/${counter}/g" .job_${filename}.cmd
+  cp .pre_process_bam_template.cmd ${TMP_DIR}/.job_${filename}.cmd
+  sed -i "s+template+$bam_file+g" ${TMP_DIR}/.job_${filename}.cmd ## Sed can use any separator rather than '/'
+  sed -i "s/filename/${filename}/g" ${TMP_DIR}/.job_${filename}.cmd
+  sed -i "s/number/${counter}/g" ${TMP_DIR}/.job_${filename}.cmd
   let counter++
 
-done < .edited_samples.txt
+done < ${TMP_DIR}/.edited_samples.txt
 
-rm -f .edited_samples.txt
+rm -f ${TMP_DIR}/.edited_samples.txt
 
 ## Launch all jobs
-for job in .job*; do sbatch $job; done
+for job in ${TMP_DIR}/.job*; do sbatch $job; done
 
 echo -e "${green}INFO${clear}    Done!"
 
 printf END >&2; uptime >&2
+
